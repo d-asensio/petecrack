4c05fd13f1a5ac12c7b5e14546cc59c310b243d9
David Asensio Cañas <david.asensio@icloud.com>
2023-09-19 09:22:40 +0200

    Clean up code

--- src/app/calculator.tsx
+++ src/app/calculator.tsx
@@ -48,34 +48,6 @@ const cylinders = [
   }
 ]
 
-const fromDepthToAmbientPressure =
-  (depth: number) =>
-    depth/10 + 1
-
-const fromAmbientPressureToDepth =
-  (ambientPressure: number) =>
-    (ambientPressure - 1)*10
-
-const partialPressureAtAmbientPressure =
-  (fGas: number, ambientPressure: number) =>
-    ambientPressure * fGas
-
-const ambientPressureOfFractionPartialPressure =
-  (ppGas: number, fGas: number) =>
-    ppGas/fGas
-
-const equivalentNarcoticDepth = ({fHe, fO2}: {fHe: number, fO2: number}, depth: number) => {
-  const fN2 = 1 - fHe - fO2
-
-  const ambientPressure = fromDepthToAmbientPressure(depth)
-
-  const ppAir = partialPressureAtAmbientPressure(fN2 + fO2, ambientPressure)
-
-  const eqAmbientPressure=  ambientPressureOfFractionPartialPressure(ppAir, 1)
-
-  return fromAmbientPressureToDepth(eqAmbientPressure)
-}
-
 function GasConsumptionCalculator() {
   const [sacRate, setSacRate] = React.useState(20)
 
